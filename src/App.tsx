593df559b54a0eedf1d18dea77d2e5d67d8a9437
Hafis Babayev <riderblade1994@gmail.com>
2025-10-13 10:03:14 +0400

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import WeatherCard from "./components/WeatherCard";
 
 const App = () => {
+  const key = import.meta.env.VITE_WEATHER_API_KEY;
+
+  useEffect(() => {
+    async function getWeather() {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=Baku&appid=${key}&units=metric`
+      );
+      const data = await res.json();
+      console.log("Погода:", data);
+    }
+
+    getWeather();
+  }, []);
+
   return (
     <>
       <WeatherCard />
