1d17c21e33eb90ac425c5be7362f27732c10352b
Hafis Babayev <riderblade1994@gmail.com>
2025-10-20 11:48:35 +0400

    create weather app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import WeatherBackground from "./components/WeatherBackground";
 import WeatherCard from "./components/WeatherCard";
 
 const App = () => {
   return (
-    <>
+    <WeatherBackground>
       <WeatherCard />
-    </>
+    </WeatherBackground>
   );
 };
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,29 @@
 import { useState, type FormEvent } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { useWeather } from "../context/weatherContext ";
+import { BeatLoader } from "react-spinners";
+import { Bounce, toast } from "react-toastify";
 
 const Sidebar = () => {
   const [title, setTitle] = useState<string>("");
-  const { weather, loading, error, getWeather } = useWeather();
+  const { weather, loading, getWeather } = useWeather();
 
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     try {
       const data = await getWeather(title);
-      if (data) {
-        // alert("We found your city");
-        console.log(weather);
+      if (!data) {
+        toast.error("City not found", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
       }
       setTitle("");
     } catch (e) {
@@ -23,13 +34,13 @@ const Sidebar = () => {
   if (loading) {
     return (
       <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
-        loading
+        <BeatLoader />;
       </div>
     );
   }
 
   return (
-    <div className="absolute h-[75%] sm:h-screen sm:w-[45%] sm:rounded-tr-none sm:rounded-bl-xl bottom-0 right-0 rounded-tl-xl rounded-tr-xl bg-white/10 backdrop-blur-lg p-4 w-full shadow-lg text-gray-300 ">
+    <div className="sm:absolute min-h-screen mt-20 sm:h-screen sm:w-[45%] sm:rounded-tr-none sm:rounded-bl-xl bottom-0 right-0 rounded-tl-xl rounded-tr-xl bg-white/10 backdrop-blur-lg p-4 w-full shadow-lg text-gray-300 ">
       <form
         onSubmit={handleSubmit}
         className="relative max-w-[350px] mx-auto flex justify-center flex-col items-center py-10"
@@ -55,19 +66,19 @@ const Sidebar = () => {
           <div className="flex items-center justify-between">
             <span className="font-medium">Feels like</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.main.feels_like.toFixed(0)}°C
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="font-medium">Humidity</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.main.humidity}%
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="font-medium">Pressure</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.main.pressure}
             </span>
           </div>
         </div>
@@ -76,19 +87,19 @@ const Sidebar = () => {
           <div className="flex items-center justify-between">
             <span className="font-medium">Country</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.sys.country}
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="font-medium">Weather</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.weather[0].main}
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="font-medium">Description</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.weather[0].description}
             </span>
           </div>
         </div>
@@ -97,7 +108,7 @@ const Sidebar = () => {
           <div className="flex items-center justify-between">
             <span className="font-medium">Wind</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.wind.speed.toFixed(0)} m/s
             </span>
           </div>
         </div>
@@ -106,13 +117,13 @@ const Sidebar = () => {
           <div className="flex items-center justify-between">
             <span className="font-medium">Max temperature</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.main.temp_max.toFixed(0)} °C
             </span>
           </div>
           <div className="flex items-center justify-between">
             <span className="font-medium">Min temperature</span>
             <span className="flex items-center font-semibold gap-0.5">
-              21 °C
+              {weather?.main.temp_min.toFixed(0)} °C
             </span>
           </div>
         </div>
